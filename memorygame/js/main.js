353acb3d5255e868dfddbe05de1b2cb9b56bc290
DallasLind <mcgraw.dallas@gmail.com>
2019-09-12 02:47:29 -0400

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,7 +1,14 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped" + cardThree);
-console.log("User flipped" + cardTwo);
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = cardsInPlay[0];
+const cardTwo = cardsInPlay[1];
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+cardsInPlay;
+ if (cardsInPlay.length === cardsInPlay[0]) {
+ 	alert("You found a match!");
+ } else {
+ 	alert("Sorry try again");
+ }
+console.log("User flipped queen");
+console.log("User flipped king");
